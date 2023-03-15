28c6b91d08dbf17ee945153ffe562dc740f4e414
sasha <you@example.com>
2023-03-15 14:39:47 +0300

    Task 1_0 ver 03

---
+++ lesson_P/1_0/1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите ваше число)");
+
+int a =int.Parse(Console.ReadLine()!);
+
+if (a== 1)
+{
+   Console.WriteLine("Mondey");
+}
+else if (a == 2)
+{
+   Console.WriteLine("Tuesday");
+}
+else if (a == 3)
+{
+   Console.WriteLine("Wednesday");
+}
+else if (a == 4)
+{
+   Console.WriteLine("Thursday");
+}
+else if (a == 5)
+{
+   Console.WriteLine("Friday");
+}
+else if (a == 6)
+{
+   Console.WriteLine("Saturday");
+}
+else if (a == 7)
+{
+   Console.WriteLine("Sunday");
+}
+else 
+{
+   Console.WriteLine("false");
+}
